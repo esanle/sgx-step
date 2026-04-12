2c47df6dd24fb0e1bbb78cd438b5bd22ef500045
zfm <2506609861@qq.com>
2026-04-11 23:48:52 -0400

    update config for xeon 5515

--- libsgxstep/config.h
+++ libsgxstep/config.h
@@ -47,9 +47,9 @@
     #define APIC_CONFIG_MSR         0
 #endif
 
-#define VICTIM_CPU                  10
-#define NUM_CORES                   16
-#define SPY_CPU                     2
+#define VICTIM_CPU                  2
+#define NUM_CORES                   32
+#define SPY_CPU                     18
 
 /*
  * XXX Configure APIC timer interval for next interrupt.
