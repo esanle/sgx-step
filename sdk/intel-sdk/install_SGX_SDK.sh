4a4042c93052b28f5accfb95b8547c0138bac1eb
esenle <EiddleChen1008@gmail.com>
2026-09-23 16:42:15 +0800

    2020623  临时备份,可能改动不需要,但是实验 6.x.x 已经成功,因此保留

--- sdk/intel-sdk/install_SGX_SDK.sh
+++ sdk/intel-sdk/install_SGX_SDK.sh
@@ -28,7 +28,7 @@ echo "SGX-SDK successfully patched!"
 
 # ----------------------------------------------------------------------
 echo "[ installing prerequisites ]"
-sudo apt-get -yqq install build-essential ocaml ocamlbuild automake autoconf libtool wget python2 libssl-dev git cmake perl
+sudo apt-get -yqq install build-essential ocaml ocamlbuild automake autoconf libtool wget python3 python-is-python3 libssl-dev git cmake perl
 sudo apt-get -yqq install libssl-dev libcurl4-openssl-dev protobuf-compiler libprotobuf-dev debhelper cmake reprepro unzip lsb-release libelf-dev
 
 OS_ID=$(lsb_release -si | tr '[:upper:]' '[:lower:]')
